a10ee0dcab442fc1e8ecb50512ccf3ca6bded13e
rocketpie <mail.l.conrad@gmail.com>
2018-01-19 21:45:26 +0100

    new solution with web API project

---
+++ MMHTT.HttpApi/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MMHTT.Api")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MMHTT.Api")]
+[assembly: AssemblyCopyright("Copyright © 2018 Lars Conrad")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("49f860f1-b1cc-4498-877c-74d422d5425b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("0.1.*")]                                                   
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# MMHTT
+# MMHTT - Massively Multithreaded Http Testing Tool
+
+or massively multithreaded online request procedural generator? 
+
